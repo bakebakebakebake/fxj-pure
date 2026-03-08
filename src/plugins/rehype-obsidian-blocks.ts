ef8022177aa07b6f677c7b39c9ea2864ca5adf45
bakebakebakebake <fxjgo@qq.com>
2026-03-09 01:49:49 +0800

    修改了使用 callout 语法的时候，md 渲染的异常

--- src/plugins/rehype-obsidian-blocks.ts
+++ src/plugins/rehype-obsidian-blocks.ts
@@ -172,10 +172,10 @@ function createCollapse(
   open: boolean
 ) {
   return h(
-    `details.obsidian-collapse.not-prose.my-4.rounded-xl.border.px-3.py-2.sm:px-4.aside-${type}`,
+    `details.obsidian-collapse.my-4.overflow-hidden.rounded-xl.border.px-3.py-2.sm:px-4.aside-${type}`,
     open ? { open: true } : {},
     [
-      h('summary.obsidian-collapse-summary.cursor-pointer.font-medium', title),
+      h('summary.obsidian-collapse-summary.not-prose.cursor-pointer.font-medium', title),
       h('div.obsidian-collapse-body.mt-3', bodyChildren)
     ]
   ) as RootContent
